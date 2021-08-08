59e6f884a15f1c28dd1c41f85ba8bbbfed7f07e8
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2021-08-08 11:21:01 +0900

    add Clock

---
+++ js/clock.js
@@ -0,0 +1,6 @@
+const clock = document.querySelector("h2 #clock");
+
+function timeRefresh() {
+    const realTime = new Date();
+    clock.innerText = `${realTime.getHours()}:${realTime.getMinutes()}`;
+}
